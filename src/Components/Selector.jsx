1223d2e861a92e92035dcbd77e7fa7666172a4a8
Kevin Bandison <kbandison@gmail.com>
2023-04-21 00:30:05 -0400

    completed product search, fixed cart, wishlist and orders page, and added orders and users pages in admin

--- src/Components/Selector.jsx
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "reselect";
-
-const getCart = (state) => state.cart;
-
-export const memorizedCart = createSelector([getCart], (cart) => cart);
-
-export default memorizedCart;
